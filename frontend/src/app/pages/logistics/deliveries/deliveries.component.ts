544aec0a335fb2378e9ec438b6616adf85a276ba
Your Name <samlothathirak@gmail.com>
2026-01-10 16:21:49 +0700

    add new deliveries page but not fully work

--- frontend/src/app/pages/logistics/deliveries/deliveries.component.ts
+++ frontend/src/app/pages/logistics/deliveries/deliveries.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-deliveries',
-  template: `<h1 class="text-2xl font-bold">Deliveries</h1>
-             <p>Track incoming and outgoing deliveries.</p>`,
-  styles: [``]
+  templateUrl: './deliveries.component.html',
 })
 export class DeliveriesComponent {}
